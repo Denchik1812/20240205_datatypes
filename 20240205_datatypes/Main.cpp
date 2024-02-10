96f73b65a5d759e6b9ab9b287ccc61a462415840
Denchik1812 <robertspidvagon1337@gmail.com>
2024-02-10 19:07:23 +0300

    Сколько времени до полуночи

--- 20240205_datatypes/Main.cpp
+++ 20240205_datatypes/Main.cpp
@@ -1,10 +1,14 @@
+/*Пользователь вводит с клавиатуры время в секундах, про -
+шедшее с начала дня.Вывести на экран текущее время в часах,
+минутах и секундах.Посчитать, сколько часов, минут и секунд
+осталось до полуночи.*/
 #include <iostream>
 #include <string>
 using namespace std;
-
 int main(int arc, char** argv) {
-	string name;
-	cout << "What's your name&\n";
-	cin >> name;
-	return 0;
+	int num;
+	cout << "Enter your time in seconds" << endl;
+	cin >> num;
+	int num2 = 86400 - num;
+	cout << "How much time is left before midnight: " << endl << num2 / 3600 << ":" << num2 % 3600 / 60 << ":" << num2 % 3600 % 60 % 60;
 }
