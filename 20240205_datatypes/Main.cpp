cd368573d46eef4e2c2b6494605c8854bc35961f
Denchik1812 <robertspidvagon1337@gmail.com>
2024-02-10 19:35:03 +0300

    Угадать оператора по коду

--- 20240205_datatypes/Main.cpp
+++ 20240205_datatypes/Main.cpp
@@ -1,14 +1,19 @@
-/*Пользователь вводит с клавиатуры время в секундах, про -
-шедшее с начала дня.Вывести на экран текущее время в часах,
-минутах и секундах.Посчитать, сколько часов, минут и секунд
-осталось до полуночи.*/
+/*Угадать оператора по коду:
+Если МТС (916, 910)
+Если Билайн (906, 902, 901, 905)
+Если Мегафон (926, 925)
+МГТС (499, 498)*/
 #include <iostream>
 #include <string>
 using namespace std;
 int main(int arc, char** argv) {
 	int num;
-	cout << "Enter your time in seconds" << endl;
+	cout << "Enter code: " << endl;
 	cin >> num;
-	int num2 = 86400 - num;
-	cout << "How much time is left before midnight: " << endl << num2 / 3600 << ":" << num2 % 3600 / 60 << ":" << num2 % 3600 % 60 % 60;
+	switch (num) {
+	    case 916: case 910: cout << "MTS"; break;
+	    case 906: case 902: case 901: case 905: cout << "BILAIN"; break;
+	    case 926: case 925: cout << "MEGAFON"; break;
+	    case 499: case 498: cout << "MGTS";
+	} 
 }
